f7c89892bf4951240cf38558e6dfe995ddec123b
xaanax <juanlucascuenca7@gmail.com>
2022-10-11 21:26:10 -0300

    avances del carrito de compras

--- utils/js/main.js
+++ utils/js/main.js
@@ -1,18 +1,94 @@
-const moreInfo = document.querySelector('#moreInfo')
-const popup = document.querySelector('#popup')
-const popupBack = document.querySelector('#popup-back')
+// E-commerce de una tienda de hardware
 
-const displayPopUp = () => {
-    console.log('gola')
-    popup.classList.add("popup")
-    popupBack.classList.add("black")
+// Creamos la Obj para crear y modificar nuestro productos
+class Product {
+    constructor(name, price, stock) {
+        this.name = name,
+        this.price = price,
+        this.stock = stock
+    }
+}
+
+
+const visor = document.querySelector('#visor')
+
+const cart = []
+
+const products = [
+    new Product('RTX 3060', 120000, true),
+    new Product('RTX 3070', 140000, false),
+    new Product('RTX 3080', 180000, true),
+    new Product('RTX 3090', 200000, false)
+]
+
+
+// renderizar productos
+const renderProducts = () => {
+    products.forEach(e  => {
+        visor.innerHTML += `
+        <div class="productItem">
+            <div class="productItem-name">${e.name}</div>
+            <div class="productItem-price">${e.price}</div>
+            <button id = "button-addCart" class="button-addCart">Add to cart</button>
+        </div>
+        `
+    });
+}
+
+renderProducts()
+
+
+
+// Agregar al carrito
+const botones = document.querySelectorAll('.button-addCart')
+
+const addToCart = (buttonIndex) => {
+    const item = products.find((e, i) => i === buttonIndex)
+    cart.push(item)
+    console.log(cart)
+}
+
+botones.forEach((e, i) => {
+    e.addEventListener('click', () => {
+        const botonIndex = i
+        addToCart(botonIndex)
+    })
+})
+
+
+
+// Render carrito
+const carrito = document.querySelector('#cart')
+
+const viewCarrito = () => {
+    const popup = document.querySelector('#popup')
+    const blackBackground = document.querySelector('#black-back')
+    console.log(popup)
+
+    popup.classList.add('cart-view')
+    popup.classList.remove('nodisplay')
     
+    blackBackground.classList.add('black-body')
+    blackBackground.classList.remove('nodisplay')
+
+    cart.forEach(e => {
+        popup.innerHTML += `
+        <div class="cart-item">
+        <div class="productItem-name">${e.name}</div>
+        <div class="productItem-price">${e.price}</div>
+    </div>
+        `
+    })
 }
 
-const delPopUp = () => {
-    popup.classList.remove("popup")
-    popupBack.classList.remove("black")
-}
+carrito.addEventListener('click', viewCarrito)
 
-moreInfo.addEventListener('click', displayPopUp)
-popupBack.addEventListener('click', delPopUp)
+
+
+
+
+
+
+
+
+
