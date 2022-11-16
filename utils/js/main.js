1e5cd43c603ff521b950e9f3b10ab74ce7d9a401
xaanax <juanlucascuenca7@gmail.com>
2022-11-16 20:47:04 -0300

    reload

--- utils/js/main.js
+++ utils/js/main.js
@@ -1,6 +1,8 @@
 
-
-
+/**
+ * Generamos el arr de los productos
+ * que tendremos en nuestra WEB
+ */
 const products = [
     {
         id: 1,
@@ -22,59 +24,66 @@ const products = [
     }
 ]
 
+
+// ----------- Obtener Dolar Blue ---------------
+
+/**
+ * Funcion para obtener el precio del dolarblue al dia de hoy
+ */
 async function setItemsPrice() {
+    /**
+     * Llamamos a la api de dolarsi para
+     * obtener el valor del dolarblue al dia de hoy
+     */
     const result = await fetch("https://www.dolarsi.com/api/api.php?type=valoresprincipales")
     const dolares = await result.json()
     const dolarBlue = parseInt(dolares[1].casa.compra) 
 
+    /**
+     * Multiplicamos el precio de los productos
+     * por el valor del dolarblue
+     */
     products.forEach(e => {
         e.price = e.price * dolarBlue
     })
 
     renderAllProducts()
 }
-
 setItemsPrice()
 
+// ----------- ./Obtener Dolar Blue ---------------
+
+
+
+// ----------- LocalSotrage ---------------
+const itemContainer = document.querySelector('#carritoModal-itemsContainer')
+const iconCartCant  = document.querySelector('#cart-cantidad')
+
+
+/**
+ * Array para guardar todos los items
+ * que el usuario introduce en el carrito
+ * de compras
+ */
+const cart = JSON.parse(localStorage.getItem("carrito")) || []
+
+
+/**
+ * Funcion para guardar en el localsotrage
+ * los items que el usuario va agregando
+ * al carrito, y los mantiene si este
+ * cierra la sesion.
+ */
 const saveLocal = () => {
     localStorage.setItem("carrito", JSON.stringify(cart))
 }
 
-const cart = JSON.parse(localStorage.getItem("carrito")) || []
-
-// ----------- Renderizar los productos ---------------
-
-const renderProduct = (_, element) => {
-
-    const productsView = document.querySelector('#productsView')
-    const renderTheProduct = 
-        productsView.innerHTML += `
-            <div class="productItem">
-                <img class="productItem__img" src="${element.imgUrl}" alt="">
-                <div class="productItem__info">$ ${element.price}</div>
-                <div class="productItem__info">${element.name}</div>
-                <button class="productItem__button">Agregar al carrito</button>
-            </div>
-        `
-
-    return renderTheProduct
-}
-
-const renderAllProducts = () => {
-    products.forEach(element  => {
-        renderProduct(products, element)
-    });
-}
-
-// ----------- ./Renderizar los productos ---------------
-
-
-
-// ----------- Render Product In Cart -----------------
-const itemContainer = document.querySelector('#carritoModal-itemsContainer')
-const iconCartCant = document.querySelector('#cart-cantidad')
-
-
+/**
+ *  Funcion que toma los datos del carrito
+ *  y cuando abrimos el mismo renderiza los 
+ *  productos que tenia el usuario antes de 
+ *  abandonar la sesion
+ */
 const recoverCart = () => {
     if (cart != []) {
     cart.forEach(element => {
@@ -89,6 +98,12 @@ const recoverCart = () => {
     }
 }
 
+/**
+ * Toma los datos guarado en la ultima sesion
+ * del usuario sobre la cantidad de items
+ * que tenia en el carrito y los usa para
+ * volverlo a mostrar al usuario
+ */
 const recoverCartCant = () => {
     if (JSON.parse(localStorage.getItem('carritoLenght') === null )) {
         iconCartCant.textContent = 0;
@@ -100,7 +115,124 @@ const recoverCartCant = () => {
 recoverCartCant()
 recoverCart()
 
+// ----------- ./LocalSotrage ---------------
+
+
+// ----------- Renderizar los productos ---------------
+
+/**
+ * Funcion para renderizar
+ * un producto que nosotros
+ * le enviaremos por parametro
+ */
+const renderProduct = (producto) => {
+    const productsView     = document.querySelector('#productsView')
+    const renderTheProduct = 
+        productsView.innerHTML += `
+            <div class="productItem">
+                <img class="productItem__img" src="${producto.imgUrl}" alt="">
+                <div class="productItem__info">$ ${producto.price}</div>
+                <div class="productItem__info">${producto.name}</div>
+                <button class="productItem__button">Agregar al carrito</button>
+            </div>
+        `
+}
+
+/**
+ * Funcion encargada de renderizar
+ * con renderProduct(), todos los 
+ * items que se encuentren en el array
+ * products
+ */
+const renderAllProducts = () => {
+    products.forEach(element  => {
+        renderProduct(element)
+    });
+}
+
+
+
+// ----------- ./Renderizar los productos ---------------
+
+
+
+// ----------- Carrito de compras -----------------
+const iconCart = document.querySelector('#cart')
+const removeCartButton = document.querySelector('#carritoModal-deleteCart')
+
+
+
+const addItemToCart = (buttonIndex) =>  {
+
+    /**
+     * La cantidad de items que tendremos
+     * dentro del carrito va a ser igual
+     * a el la longitud de nuestro array
+     * cart + 1
+     */
+    let itemsInCart = (cart.length + 1)
+    /** 
+     * Guardamos la cantidad de items en el
+     * carrito en el localstorage para luego
+     * mantener el dato
+     */
+    localStorage.setItem('carritoLenght', JSON.stringify(itemsInCart))
+
+    const incrementItemInCar = () => {
+        iconCartCant.textContent = JSON.parse(localStorage.getItem('carritoLenght'))
+    }
+
+
+    const isValidItem = products.find((_, productIndex) => productIndex === buttonIndex)
+
+
+    const addProducInCart = () => {
+        cart.push(isValidItem)
+        renderProductInCart(isValidItem)
+        saveLocal()
+    }
+    
+
+    if (isValidItem) {
+        addProducInCart()
+        incrementItemInCar()
+        console.log(cart)
+    }
+}
+
+const removeAllItemsFromCart = () => {
+    if(cart.length < 1) {
+        swal({
+            title: "No tienes nada en el carrito",
+            text: "Que quieres borrar?",
+            icon: "error",
+        });
+    } else {
+        cart.length = 0
+        localStorage.clear()
+        itemContainer.innerHTML = ``
+        iconCartCant.textContent = 0
+        openAndCloseModalCart("close")
+
+        swal({
+            title: "Borraste el carrito",
+            text: "Excelente!",
+            icon: "success",
+        });
+    }
+
+
+}
+
+removeCartButton.addEventListener('click', removeAllItemsFromCart)
+
+/**
+ * Renderiza un elemento en el carrito
+ * de compras cuando el usuario agrega
+ * uno
+ */
 const renderProductInCart = (item) => {
+
     itemContainer.innerHTML += `
     <div class="item">
     <img class="item-img" src="${item.imgUrl}" alt="">
@@ -108,38 +240,6 @@ const renderProductInCart = (item) => {
     <div class="item-price">$ ${item.price}</div>
     </div>
     `
-}
-// ----------- ./Render Product In Cart -----------------
-
-
-
-// ----------- Agregar al carrito ---------------
-
-const iconCart = document.querySelector('#cart')
-let itemsInCart;
-
-const addItemToCart = (buttonIndex) =>  {
-    let itemsInCart = (cart.length + 1)
-
-    const setCantCarritoInLocalStorage = localStorage.setItem('carritoLenght', JSON.stringify(itemsInCart))
-
-    const isValidItem = products.find((_, productIndex) => productIndex === buttonIndex)
-
-    const incrementItemInCar = () => {iconCartCant.textContent = JSON.parse(localStorage.getItem('carritoLenght'))}
-
-    const addProducInCart = () => {
-        cart.push(isValidItem)
-        renderProductInCart(isValidItem)
-        saveLocal()
-
-    }
-    
-    if (isValidItem) {
-        addProducInCart()
-        incrementItemInCar()
-        console.log(cart)
-
-    }
 }
 
 setTimeout(() => {
@@ -150,13 +250,20 @@ setTimeout(() => {
     })
 }, 2000)
 
-// ----------- ./Agregar al carrito ---------------
+// ----------- ./Carrito de compras -----------------
 
 
 
-// ------------------ Open Moda Cart ------------------
+// ------------------ Ventana modal (carrito de compras) ------------------
+
 const closeModal = document.querySelector('#carritoModal-close')
 
+/**
+ * Funcion que depende del parametro
+ * que el enviaremos en un evento a-
+ * brira o cerrara la el modal del
+ * carrito de compras
+ */
 const openAndCloseModalCart = (action) => {
 
     const carritoModal         = document.querySelector('#carritoModal')
@@ -181,13 +288,10 @@ const openAndCloseModalCart = (action) => {
     }
 }
 
-iconCart.addEventListener('click', () => {
-    openAndCloseModalCart("open")
-
-})
-
+iconCart.addEventListener('click', () => {openAndCloseModalCart("open")})
 closeModal.addEventListener('click', () => openAndCloseModalCart("close"))
-// ------------------ ./Open Moda Cart ------------------
+
+// ------------------ ./Ventana modal (carrito de compras) ------------------
 
 
 
